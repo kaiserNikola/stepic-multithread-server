bd35baaacfd607199d0222d845ccf8d1c79b02e0
kaiser <kaiserNikola@yandex.ru>
2016-02-28 19:47:32 +0300

    support for GET params after ? bugfix

--- main.cpp
+++ main.cpp
@@ -68,8 +68,7 @@ void reply_send_http(int sockfd, int code, const string& msg){
 	if (code == 404){
 		send_reply_str(sockfd, "HTTP/1.0 404 NOT FOUND\r\n");	
 		send_reply_str(sockfd, "Content-Type: text/html\r\n");	
-		send_reply_str(sockfd, "\r\n");		 
-		send_reply_str(sockfd, "<h1>Page not found!<h1>\r\n<i>404 desu yo!</i>");		
+		send_reply_str(sockfd, "\r\n");		 	
 	}
 	log("---\n");
 }
